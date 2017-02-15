1300f56f72334533d9e234d983283f00e5c678a2
hi_man_shoe <hello2himanshusingh@gmail.com>
2017-02-15 09:28:58 +0530

    Profile + Hobby

--- app/src/main/java/com/portfolio/vidisha/Graduation/Contact.java
+++ app/src/main/java/com/portfolio/vidisha/Graduation/Contact.java
@@ -1,15 +1,65 @@
 package com.portfolio.vidisha.Graduation;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.portfolio.vidisha.R;
 
 public class Contact extends AppCompatActivity {
+    public static String FACEBOOK_URL = "https://www.facebook.com/YourPageName";
+    public static String FACEBOOK_PAGE_ID = "YourPageName";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
+        getSupportActionBar().setTitle("Contact me");//toolbar name
+    }  public void onHome(View v){
+
+        startActivity(new Intent(Contact.this,Home.class));
+    }
+    public void onSchool(View v){
+
+        startActivity(new Intent(Contact.this,Graduation.class));
+    }
+    public void onHobby(View v){
+
+        startActivity(new Intent(Contact.this,Hobby.class));
+    }
+    public void onContact(View v){
+
+        startActivity(new Intent(Contact.this,Contact.class));
+    }
+    public void onCall(View v){
+
+        Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + "9714522455"));
+        startActivity(intent);
+    }
+    public void onSnapchat(View v){
+
+       
+    }
+    public void onFacebook(View v){
+
+        startActivity(new Intent(Contact.this,Home.class));
+    }
+    public void onInsta(View v){
+
+        startActivity(new Intent(Contact.this,Home.class));
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
     }
 }
+
+
--- app/src/main/java/com/portfolio/vidisha/Graduation/Hobby.java
+++ app/src/main/java/com/portfolio/vidisha/Graduation/Hobby.java
@@ -13,6 +13,34 @@ public class Hobby extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hobby);
+        getSupportActionBar().setTitle("Skills");//toolbar name
+    }  public void onHome(View v){
 
+        startActivity(new Intent(Hobby.this,Home.class));
+    }
+    public void onSchool(View v){
+
+        startActivity(new Intent(Hobby.this,Graduation.class));
+    }
+    public void onHobby(View v){
+
+        startActivity(new Intent(Hobby.this,Hobby.class));
+    }
+    public void onContact(View v){
+
+        startActivity(new Intent(Hobby.this,Contact.class));
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
     }
 }
+
+
+
+
